51e9917dc5f99d8b0a3f5b52d38fe2c17cd49a5f
Heather Izumi <hkizumi@gmail.com>
2019-04-15 16:22:36 -0700

    Added Drivers Passengers Trips Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'trips/show'
+  get 'trips/new'
+  get 'trips/create'
+  get 'trips/edit'
+  get 'trips/update'
+  get 'trips/delete'
+  get 'drivers/index'
+  get 'drivers/show'
+  get 'drivers/new'
+  get 'drivers/create'
+  get 'drivers/edit'
+  get 'drivers/update'
+  get 'drivers/delete'
+  get 'passengers/index'
+  get 'passengers/show'
+  get 'passengers/new'
+  get 'passengers/create'
+  get 'passengers/edit'
+  get 'passengers/update'
+  get 'passengers/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
