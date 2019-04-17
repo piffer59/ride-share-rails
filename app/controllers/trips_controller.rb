01e969e8f39316cbc69b9b4aec0d2598fe43f608
Shamira <marshallscm@gmail.com>
2019-04-17 14:44:58 -0700

    Added Trip Controller methods for edit, update, and destroy.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,14 +6,48 @@ class TripsController < ApplicationController
   end
 
   def create
+    @trip = Trip.new(trip_params)
+
+    is_successful = @trip.save
+
+    if is_successful
+      redirect_to trip_path(@trip.id)
+    else
+      render :new, status: :bad_request
+    end
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
   end
 
   def update
+    trip = Trip.find_by(id: params[:id])
+
+    is_successful = trip.update(trip_params)
+
+    if is_successful
+      redirect_to trip_path(trip.id)
+    else
+      @trip = trip
+      render :edit, status: :bad_request
+    end
   end
 
-  def delete
+  def destroy
+    trip = Trip.find_by(id: params[:id])
+
+    if trip.nil?
+      head :not_found
+    else
+      trip.destroy
+      redirect_to books_path
+    end
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
   end
 end
