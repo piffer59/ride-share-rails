4468f38ca57699237d397fa5bebcee3dbabd2a10
Shamira <marshallscm@gmail.com>
2019-04-16 20:51:36 -0700

    Added form template for driver and driver tests, index, show, and new.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,11 +1,18 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def show
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+    if @driver.nil?
+      head :not_found
+    end
   end
 
   def new
+    @driver = Driver.new(name: "Name")
   end
 
   def create
--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,25 +1,59 @@
 require "test_helper"
 
 describe DriversController do
+  let (:driver) {
+    Driver.create name: "Ridley", vin: "R1CKRVH55W8S6S9T9"
+  }
   describe "index" do
-    it "can get index" do
-      # Your code here
+    it "can get the index path" do
+      # Act
+      get drivers_path
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "can get the root path" do
+      # Act
+      get root_path
+
+      # Assert
+      must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "can get a valid Driver" do
+
+      # Act
+      get driver_path(driver.id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid driver" do
+
+      # Act
+      get driver_path(-1)
+
+      # Assert
+      must_respond_with :not_found
+    end
   end
 
+  describe "new" do
+    it "can get the new driver page" do
+      get new_driver_path
+
+      must_respond_with :success
+    end
+  end
   describe "edit" do
     # Your tests go here
   end
 
   describe "update" do
-    # Your tests go here
-  end
-
-  describe "new" do
     # Your tests go here
   end
 
