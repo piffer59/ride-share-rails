4d73a642e7da73e1c45e0c52936116e7ea1c3e4b
Shamira <marshallscm@gmail.com>
2019-04-16 17:03:12 -0700

    Added routes for Driver Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "passengers#index"
+  root to: "drivers#index"
 
   resources :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :drivers
 end
