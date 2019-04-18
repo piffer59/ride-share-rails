a58db01abfc860398f3c8f7da4a645ef756f9f76
Heather Izumi <hkizumi@gmail.com>
2019-04-18 14:54:20 -0700

    Passenger model trying to list all trips for a passenger, passenger show

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,6 +4,14 @@ class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_num, presence: true
 
+  def list_passenger_trips
+    money = 0
+    self.trips.each do |trip|
+      puts "hello"
+      # trip.driver_id
+    end
+  end
+
   def charges
     money = 0
     self.trips.each do |trip|
