c4c5a683bab5e6ca0859c4fca8b351cc28821e99
Shamira <marshallscm@gmail.com>
2019-04-18 14:16:07 -0700

    Added Passenger model method and formatting for view.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,10 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def charges
+    self.trips.each do |trip|
+      trip.cost
+    end
+  end
 end
